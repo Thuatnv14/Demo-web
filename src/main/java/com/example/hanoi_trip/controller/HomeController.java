bb64a884934193b36756118d646919d0fb35d1ba
thang <huuthang6101997@gmail.com>
2024-10-19 23:00:53 +0700

    run ok roi nha

--- src/main/java/com/example/hanoi_trip/controller/HomeController.java
+++ src/main/java/com/example/hanoi_trip/controller/HomeController.java
@@ -5,16 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @Autowired
     private PlaceService place_service;
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String homePage(Model model) {
         model.addAttribute("title", "Welcome to Ha Noi Trip");
         model.addAttribute("places", place_service.getAllPlaces());
-        return "home"; // Đảm bảo tên này khớp với file home.html
+        return "home";
     }
 }
